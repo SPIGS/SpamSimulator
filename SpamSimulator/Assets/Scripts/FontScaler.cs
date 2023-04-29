b16cfafeefd95051bc4cc96720914e4387169a91
SPIGS <paspielvogel@gmail.com>
2023-04-29 16:30:53 -0400

    Toolbar styling font scaler.

---
+++ SpamSimulator/Assets/Scripts/FontScaler.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class FontScaler : MonoBehaviour
+{
+    private float fontScale = 1.0f;
+    private VisualElement root;
+
+    void Start() {
+        float playableHeight = Screen.height;
+        fontScale = playableHeight / 1080;
+
+        root = GetComponent<UIDocument>().rootVisualElement;
+        scaleFont(root);
+
+        // Super hardcoded stuff
+        VisualElement fileDropdown = root.Query<VisualElement>("FileDropdown");
+        fileDropdown.style.left = 10 * fontScale - fontScale *fontScale;
+        VisualElement settingsDropdown = root.Query<VisualElement>("SettingsDropdown");
+        settingsDropdown.style.left = 10 * fontScale + (55 - fontScale) * fontScale - fontScale * fontScale;
+        VisualElement toolsDropdown = root.Query<VisualElement>("ToolsDropdown");
+        toolsDropdown.style.left = 10 * fontScale + (160-fontScale) * fontScale - fontScale * fontScale;
+    }
+
+    public void scaleFont (VisualElement element) {
+        if (element.childCount == 0) {
+            element.style.fontSize = 24 * fontScale;
+            
+            if (element.style.borderBottomWidth.value > 0) {
+                element.style.borderBottomWidth = fontScale * fontScale;
+            }
+            if (element.style.borderLeftWidth.value > 0)
+            {
+                element.style.borderLeftWidth = fontScale * fontScale;
+            }
+            if (element.style.borderTopWidth.value > 0)
+            {
+                element.style.borderTopWidth = fontScale * fontScale;
+            }
+            if (element.style.borderRightWidth.value > 0)
+            {
+                element.style.borderRightWidth = fontScale * fontScale;
+            }
+
+            // Style execptions
+            if (element.ClassListContains("toolbar-button")) {
+                element.style.marginRight = 10 * fontScale;
+            } else if (element.ClassListContains("dropdown-button")) {
+                element.style.paddingLeft = 30 * fontScale;
+                element.style.paddingRight = 30 * fontScale;
+            }else if(element.ClassListContains("dropdown")) {
+                element.style.paddingTop = 1 * fontScale;
+                element.style.paddingBottom = 1 * fontScale;
+            }
+
+            //Name execptions
+            if (element.name == "ToolbarContainer") {
+                element.style.paddingLeft = 10 * fontScale;
+                element.style.paddingRight = 10 * fontScale;
+                element.style.paddingTop = 5 * fontScale;
+                element.style.paddingBottom = 5 * fontScale;
+            } 
+
+        } else if (element.childCount > 0) {
+            foreach(VisualElement child in element.Children()) {
+                scaleFont(child);
+            }
+        }
+    }
+}
---
+++ SpamSimulator/Assets/Scripts/Toolbar.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Toolbar : MonoBehaviour
+{
+    public VisualTreeAsset aboutWindowTemplate;
+
+    private ModalWindowCreator windowCreator;
+    private VisualElement root;
+    private Button fileButton;
+    private Button settingsButton;
+    private Button toolsButton;
+    private Button aboutButton;
+
+    private enum ToolbarButtons {
+        FILE,
+        SETTINGS,
+        TOOLS,
+        ABOUT
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        windowCreator = GetComponent<ModalWindowCreator>();
+        // Get the buttons
+        root = GetComponent<UIDocument>().rootVisualElement;
+        fileButton = root.Query<Button>("FileButton");
+        settingsButton = root.Query<Button>("SettingsButton");
+        toolsButton = root.Query<Button>("ToolsButton");
+        aboutButton = root.Query<Button>("AboutButton");
+
+        //Register Callbacks
+        fileButton.clickable.clicked += () => {
+            ToggleButtonActive(ToolbarButtons.FILE);
+        };
+        settingsButton.clickable.clicked += () =>
+        {
+            ToggleButtonActive(ToolbarButtons.SETTINGS);
+        };
+        toolsButton.clickable.clicked += () =>
+        {
+            ToggleButtonActive(ToolbarButtons.TOOLS);
+        };
+        aboutButton.clickable.clicked += () =>
+        {
+            ToggleButtonActive(ToolbarButtons.ABOUT);
+        };
+
+        // Set pointer up on event
+        root.RegisterCallback<PointerUpEvent>(OnPointerUp);
+
+    }
+
+    void ToggleButtonActive (ToolbarButtons pressed) {
+
+        DeactivateButtons();
+        HideAllDropdowns();
+
+        switch (pressed)
+        {
+            case ToolbarButtons.FILE:
+                fileButton.AddToClassList("toolbar-button-selected");
+                ShowFileDropdown();
+                break;
+            case ToolbarButtons.SETTINGS:
+                settingsButton.AddToClassList("toolbar-button-selected");
+                ShowSettingsDropdown();
+                break;
+            case ToolbarButtons.TOOLS:
+                toolsButton.AddToClassList("toolbar-button-selected");
+                ShowToolsDropdown();
+                break;
+            case ToolbarButtons.ABOUT:
+                aboutButton.AddToClassList("toolbar-button-selected");
+                ShowAboutModal();
+                break;
+        }
+    }
+    void OnPointerUp (PointerUpEvent evt) {
+        // This is to handle the case where we click somewhere that isn't the toolbar
+        Debug.Log("Pointer Up");
+        DeactivateButtons();
+        HideAllDropdowns();
+    }
+
+    void DeactivateButtons() {
+        fileButton.RemoveFromClassList("toolbar-button-selected");
+        settingsButton.RemoveFromClassList("toolbar-button-selected");
+        toolsButton.RemoveFromClassList("toolbar-button-selected");
+        aboutButton.RemoveFromClassList("toolbar-button-selected");
+    }
+
+    void ShowFileDropdown () {
+        Debug.Log("File Dropdown");
+        VisualElement fileDropdown = root.Query<VisualElement>("FileDropdown");
+        fileDropdown.visible = true;
+    }
+
+    void ShowSettingsDropdown() {
+        Debug.Log("Settings Dropdown");
+        VisualElement settingsDropdown = root.Query<VisualElement>("SettingsDropdown");
+        settingsDropdown.visible = true;
+    }
+
+    void ShowToolsDropdown() {
+        Debug.Log("Tools Dropdown");
+        VisualElement toolsDropdown = root.Query<VisualElement>("ToolsDropdown");
+        toolsDropdown.visible = true;
+    }
+
+    void ShowAboutModal () {
+        Debug.Log("About Dropdown");
+        int width = 300;
+        int height = 200;
+        this.windowCreator.CreateModal(this.aboutWindowTemplate, 
+                (Screen.width/2) - width/2, 
+                (Screen.height/2) - height/2, 
+                width, 
+                height);
+    }
+
+    void HideAllDropdowns () {
+        VisualElement fileDropdown = root.Query<VisualElement>("FileDropdown");
+        fileDropdown.visible = false;
+        VisualElement settingsDropdown = root.Query<VisualElement>("SettingsDropdown");
+        settingsDropdown.visible = false;
+        VisualElement toolsDropdown = root.Query<VisualElement>("ToolsDropdown");
+        toolsDropdown.visible = false;
+    }
+}
